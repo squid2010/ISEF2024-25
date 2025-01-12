59f6bb9d9a87f6b84f79784724978f91407b8c3e
squid2010 <sumerchaudhary1@gmail.com>
2025-01-11 21:41:13 -0500

    ran everything, got results

--- control.py
+++ control.py
@@ -1,7 +1,7 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 def create_bell_state() -> QuantumCircuit:
     q = QuantumRegister(2, 'q')
-    c = ClassicalRegister(2, 'c')
+    c = ClassicalRegister(2, 'measured_output')
     qc = QuantumCircuit(q,c, name="Bell State Unencoded")
     
     #Bell State
